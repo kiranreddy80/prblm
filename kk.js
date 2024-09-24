ff13c7a778d65fd7a41a8e95aeeaba43ad8882d8
Kiran Kumar <kirankumarpilli82gmail.com>
2024-09-24 20:15:27 +0530

    hii

---
+++ kk.js
@@ -0,0 +1,33 @@
+// 1. problem one the number is positive or not
+
+let num=29
+if(num>0){
+    console.log("number is positive")
+}
+else{
+    console.log("number is negative");
+    
+}
+// grade based on the marks
+let marks=93;
+if(marks>90){
+    console.log( marks,"your grade is A");
+    
+}else if(marks>80){
+    console.log( marks,"your grade is B");
+    
+}else if(marks>=70){
+    console.log( marks,"your grade is C");
+    
+} else if(marks>=50){
+    console.log( marks,"your grade is d");
+    
+}else if(marks<40){
+    console.log( marks,"your are fail");
+    
+}
+// 3..guess the out put
+// let a = 5;
+// let b = "5";
+// console.log(a == b);...true,because check only value
+// console.log(a !== b); ..true ,because check value data type
